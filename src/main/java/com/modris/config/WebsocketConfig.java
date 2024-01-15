9dd9286ff96e87b7ec26564d1a1d94aa85bff81e
modris <modrislauzis@gmail.com>
2024-01-15 17:17:30 +0200

    RabbitMQ RCP Setup with WebSocket, STOMP, SockJS. Basic setup. No error handling, reconnection or test cases for now.

--- src/main/java/com/modris/config/WebsocketConfig.java
+++ src/main/java/com/modris/config/WebsocketConfig.java
@@ -5,7 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-/*
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer{
@@ -29,4 +29,3 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer{
 	}
 	
 }
-*/
--- src/main/java/com/modris/controller/MessageController.java
+++ src/main/java/com/modris/controller/MessageController.java
@@ -1,17 +1,35 @@
 package com.modris.controller;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
+
+import com.modris.model.Fen;
 
 @Controller
 public class MessageController {
 
-	@MessageMapping("/websocket") //don't forget /app prefix. So /app/websocket for frontend
-	@SendTo("/exchange/main_exchange")
-	public String test(String incoming) {
-		System.out.println(incoming);
-		System.out.println("r1bqkb1r/pppn1ppp/3p1n2/4p3/3PP3/2N2N2/PPP2PPP/R1BQKB1R w KQkq - 0 1");
-		return "r1bqkb1r/pppn1ppp/3p1n2/4p3/3PP3/2N2N2/PPP2PPP/R1BQKB1R w KQkq - 0 1";
+	private final RabbitTemplate rabbit;
+	private final SimpMessageSendingOperations messageTemplate;
+
+	@Autowired
+	public MessageController(RabbitTemplate rabbit, SimpMessageSendingOperations messageTemplate) {
+		this.rabbit = rabbit;
+		this.messageTemplate = messageTemplate;
 	}
+
+	@MessageMapping("/websocket") // don't forget /app prefix. So /app/websocket for frontend
+	// @SendToUser(value = "/topic/bestmove", broadcast = false) // will be
+	// /user/topic/bestmove
+	public void test(Fen fen) {
+		System.out.println("UserId: " + fen.getUserId());
+		System.out.println(fen.getFen());
+		String response = (String) rabbit.convertSendAndReceive("main_exchange", "", fen.getFen());
+		System.out.println("Received response from worker: " + response);
+		messageTemplate.convertAndSend("/topic/bestmove" + fen.getUserId(), response);
+		
+	}
+
 }
