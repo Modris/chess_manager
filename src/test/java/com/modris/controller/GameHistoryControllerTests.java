2386423ebc8272271f6cd8c5b178d92b7341e147
modris <modrislauzis@gmail.com>
2024-02-17 17:30:03 +0200

    RestControllerAdvice tests for HttpResponse 204 and 400. Mocking exceptions thrown in GameHistoryControllerTests.

--- src/test/java/com/modris/controller/GameHistoryControllerTests.java
+++ src/test/java/com/modris/controller/GameHistoryControllerTests.java
@@ -1,9 +1,14 @@
 package com.modris.controller;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,14 +18,22 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import com.modris.Main;
 import com.modris.config.SecurityConfig;
+import com.modris.exceptions.FenIsInvalidException;
+import com.modris.exceptions.GameNotFoundException;
+import com.modris.exceptions.HistoryIsEmptyException;
+import com.modris.exceptions.StatisticsNotFoundException;
+import com.modris.model.FenValidator;
 import com.modris.model.SaveGame;
 import com.modris.services.ChessService;
 
+
 @ContextConfiguration(classes={Main.class,SecurityConfig.class})
 @WebMvcTest(GameHistoryController.class)
+
 @DisplayName("GameHistoryController mapping tests:")
 public class GameHistoryControllerTests {
 
@@ -30,6 +43,9 @@ public class GameHistoryControllerTests {
 	
 	@MockBean
 	ChessService chessService;
+	
+	@MockBean
+	FenValidator fenValidator;
 	
 /*
 	@GetMapping("/get/game/{id}")
@@ -77,38 +93,44 @@ public class GameHistoryControllerTests {
 	@Test
 	@DisplayName("/save bad request 400. Incorrect color.")
 	public void saveBadRequest1()  throws Exception{
-		mockMvc.perform(post("/save")
+		MvcResult result = mockMvc.perform(post("/save")
 				.param("color", "whites") // white would be correct
 				.param("result", "Win")
 				.param("moves", "e2e5,b7b6")
 				.param("fen", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 				.param("elo","3190")
 				)
-	      .andExpect(status().isBadRequest());    
+	      .andExpect(status().isBadRequest()).andReturn();    
+		assertTrue(result.getResolvedException().getMessage().contains("must match \"black|white\""));
+		
 	}
 	@Test
 	@DisplayName("/save bad request 400. Incorrect result.")
 	public void saveBadRequest2()  throws Exception{
-		mockMvc.perform(post("/save")
+		MvcResult result = mockMvc.perform(post("/save")
 				.param("color", "white")
 				.param("result", "Wins") // Win would be correct
 				.param("moves", "e2e5,b7b6")
 				.param("fen", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 				.param("elo","3190")
 				)
-	      .andExpect(status().isBadRequest());    
+	      .andExpect(status().isBadRequest()).andReturn();    
+		assertTrue(result.getResolvedException().getMessage().contains("must match \"Draw|Win|Loss\""));
 	}
 	@Test
 	@DisplayName("/save bad request 400. Incorrect moves.")
 	public void saveBadRequest3()  throws Exception{
-		mockMvc.perform(post("/save")
+		MvcResult result = mockMvc.perform(post("/save")
 				.param("color", "white")
 				.param("result", "Win")
 				.param("moves", "e2e5, b7b6 ") // spaces
 				.param("fen", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 				.param("elo","3190")
 				)
-	      .andExpect(status().isBadRequest());    
+	      .andExpect(status().isBadRequest()).andReturn();    
+
+		assertTrue(result.getResolvedException().getMessage().contains("'moves': rejected value [e2e5, b7b6 ]"));
+		assertTrue(result.getResolvedException().getMessage().contains("must match \"([abcdefgh][1-8][abcdefgh][1-8][qnbr]?,?)+\""));
 	}
 	
 	@Test
@@ -121,20 +143,39 @@ public class GameHistoryControllerTests {
 				.param("fen", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") // z in the pawn structure
 				.param("elo","3190")
 				)
-	      .andExpect(status().isUnauthorized());    
+	      .andExpect(status().isUnauthorized());  
+		
 	}
 	
 	@Test
-	@DisplayName("/save bad request 400. Incorrect elo.")
+	@DisplayName("/save bad request 400. Incorrect elo. Exceeds Maximum 3190.")
 	public void saveBadRequest5()  throws Exception{
-		mockMvc.perform(post("/save")
+		MvcResult result = mockMvc.perform(post("/save")
 				.param("color", "white")
 				.param("result", "Win")
 				.param("moves", "e2e5,b7b6") 
 				.param("fen", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") 
 				.param("elo","3191") // elo is 3191 exceeds 3190 limit.
 				)
-	      .andExpect(status().isBadRequest());    
+	      .andExpect(status().isBadRequest()).andReturn();    
+	
+		assertTrue(result.getResolvedException().getMessage().contains("Elo must be at most 3190"));
+	}
+	
+	@Test
+	@DisplayName("/save bad request 400. Incorrect elo. Exceeds minimum 1320.")
+	public void saveBadRequest6()  throws Exception{
+		MvcResult result = mockMvc.perform(post("/save")
+				.param("color", "white")
+				.param("result", "Win")
+				.param("moves", "e2e5,b7b6") 
+				.param("fen", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") 
+				.param("elo","1319") // elo is 1319 below minium.
+				)
+	      .andExpect(status().isBadRequest()).andReturn();    
+	
+		
+		assertTrue(result.getResolvedException().getMessage().contains("Elo must be at least 1320"));
 	}
 	
 	
@@ -167,6 +208,78 @@ public class GameHistoryControllerTests {
 	public void statisticsAuthorizedOk() throws Exception{
 		mockMvc.perform(get("/statistics"))
 				.andExpect(status().isOk());
+	}
+	
+	@Test
+	@WithMockUser(username = "abc")
+	@DisplayName("/statistics AUTHORIZED. 204 no content throws StatisticsNotFoundException.")
+	public void statisticsAuthorizedNoContent() throws Exception{
+		//throw new StatisticsNotFoundException("abc");
+		when(chessService.getStats("abc")).thenThrow( new StatisticsNotFoundException("abc"));
+		MvcResult result = mockMvc.perform(get("/statistics"))
+				.andExpect(status().isNoContent()).andReturn();
+		
+		Optional<StatisticsNotFoundException> someException = Optional.ofNullable((StatisticsNotFoundException) result.getResolvedException());
+		assertEquals("Statistics for user id: `abc` not found.", result.getResolvedException().getMessage());
+
+	}
+	
+	@Test
+	@WithMockUser(username = "abc")
+	@DisplayName("/getGameHistory AUTHORIZED. 204 no content throws HistoryIsEmptyException.")
+	public void getGameHistoryNoContent() throws Exception{
+		//throw new StatisticsNotFoundException("abc");
+		when(chessService.fetchGameHistory("0", "5","abc")).thenThrow( new HistoryIsEmptyException("abc"));
+		MvcResult result = mockMvc.perform(get("/getGameHistory")
+				.param("page", "0")
+				.param("pageSize", "5"))
+				.andExpect(status().isNoContent()).andReturn();
+		
+		Optional<HistoryIsEmptyException> someException = Optional.ofNullable((HistoryIsEmptyException) result.getResolvedException());
+		assertEquals("History for user id `abc` is empty.", result.getResolvedException().getMessage());
+
+	}
+	
+	@Test
+	@WithMockUser(username = "abc")
+	@DisplayName("/get/game/abc AUTHORIZED. 204 no content throws GameNotFoundException.")
+	public void getGameIdAuthorized204NoContent() throws Exception{
+		//throw new StatisticsNotFoundException("abc");
+		when(chessService.getGameWithId("abc")).thenThrow( new GameNotFoundException("abc"));
+		MvcResult result = mockMvc.perform(get("/get/game/abc"))
+				.andExpect(status().isNoContent()).andReturn();
+		
+		Optional<GameNotFoundException> someException = Optional.ofNullable((GameNotFoundException) result.getResolvedException());
+		assertEquals("A book with game id: `abc` not found.", result.getResolvedException().getMessage());
+
+	}
+	// FenIsInvalidException
+	
+	@Test
+	@WithMockUser(username = "abc")
+	@DisplayName("/save AUTHORIZED. Incorrect FEN throws FenIsInvalidException (400 bad request)")
+	public void saveAuthorizedIncorrectFenThrowsException() throws Exception{
+
+		
+		var game = new SaveGame("Black resigns.","3189","white","h2h3,"
+				,"invalidFEN","Win");
+
+		doThrow(new FenIsInvalidException("Invalid Fen.", game.getFen()))
+        .when(chessService).saveGame(any(SaveGame.class), any(String.class));
+		
+		MvcResult result = mockMvc.perform(post("/save")
+				.param("result", game.getResult())
+				.param("elo", game.getElo())
+				.param("color", game.getColor())
+				.param("moves", game.getMoves())
+				.param("fen", game.getFen())
+				.param("winner", game.getWinner()))
+				.andExpect(status().isBadRequest()).andReturn();
+		
+		
+		Optional<FenIsInvalidException> someException = Optional.ofNullable((FenIsInvalidException) result.getResolvedException());
+		assertEquals("Invalid Fen.. Fen: invalidFEN", result.getResolvedException().getMessage());
+
 	}
 	
 }
